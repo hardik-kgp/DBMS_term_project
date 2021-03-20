3d5e603f3fd7c899fa5d0c8c37533416dbe84a74
iakshat <akshat1118@gmail.com>
2021-03-20 20:53:04 +0530

    orders create done

--- resauto/__init__.py
+++ resauto/__init__.py
@@ -3,9 +3,20 @@ from django.db import connection
 def setup_database():
     cursor = connection.cursor()
     #create table queries
-    orders_query = """CREATE TABLE IF NOT EXISTS orders_test(
-                        order_id INT(10)
-                    )"""
+    orders_query = """CREATE TABLE IF NOT EXISTS orders(
+                        order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        order_time DATETIME,
+                        order_status VARCHAR,
+                        order_type VARCHAR,
+                        customer_id INTEGER,
+                        address_id INTEGER,
+                        payment_method VARCHAR,
+                        total_bill INTEGER,
+                        rating INTEGER,
+                        feedback VARCHAR,
+                        FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+                        FOREIGN KEY (address_id) REFERENCES customer_address(address_id)
+                    );"""
 
     #execute all crete queries
     cursor.execute(orders_query)
